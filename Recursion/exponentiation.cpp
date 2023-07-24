e224084de4f1cb2011fb4b1241991bb35f410cee
Rishabh <rishabhverma.1202@gmail.com>
2023-07-24 19:27:11 +0530

    Exponentiation

--- Recursion/exponentiation.cpp
+++ Recursion/exponentiation.cpp
@@ -1,20 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int expo(int n){
-    if(n == 0) return 1;
-    if(n == 1) return 2;
-    if(n&1){
-        return 2* expo(n/2) * expo(n/2);
+int expo(int n, int p){
+    if(p == 0) return 1;
+    if(p == 1) return n;
+    if(p&1){
+        return n* expo(n,p/2) * expo(n,p/2);
     }
-    else return expo(n/2) * expo(n/2);
+    else return expo(n,p/2) * expo(n,p/2);
 }
 
 int main(){
-    int n; 
-    cin>>n;
-    
-    int ans = expo(n);
+    int n,p; 
+    cin>>n>>p;
+
+    int ans = expo(n,p);
     cout<<ans<<endl;
     return 0;
 }
