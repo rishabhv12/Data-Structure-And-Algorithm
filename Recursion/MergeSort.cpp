25803b1fd304164cf141e08ba221ab550b137056
Rishabh <rishabhverma.1202@gmail.com>
2023-12-29 23:50:36 +0530

    Merge sort

--- Recursion/MergeSort.cpp
+++ Recursion/MergeSort.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void merge(int *ar, int s, int e){
+void merge(vector<int> &ar, int s, int e){
     int mid = s+(e-s)/2;
 
     int len1 = mid-s+1;
@@ -43,8 +43,34 @@ void merge(int *ar, int s, int e){
 
 }
 
+// merge(ar, s, mid, e);
+void merge(vector<int> &ar, int s, int mid, int e){
+    vector<int> temp;
 
-void mergeSort(int *ar, int s, int e){
+    int left = s;
+    int right = mid+1;
+
+    while(left <= mid && right<=e){
+        if(ar[left]<=ar[right]){
+            temp.push_back(ar[left++]);
+        }
+        else temp.push_back(ar[right++]);
+    }
+
+    while(left <= mid){
+        temp.push_back(ar[left++]);
+    }
+    
+    while(right <= e){
+        temp.push_back(ar[right++]);
+    }
+
+    for(int i=s; i<=e;i++){
+        ar[i] = temp[i-s];
+    }
+}
+
+void mergeSort(vector<int> &ar, int s, int e){
     
     if(s>=e) return;
 
@@ -56,12 +82,13 @@ void mergeSort(int *ar, int s, int e){
     // sort right part of array
     mergeSort(ar, mid+1, e);
 
-    merge(ar, s,e);
+    // merge(ar, s,e);
+    merge(ar, s, mid, e);
 }
 
 int main(){
     int n; cin>>n;
-    int ar[n];
+    vector<int> ar(n);
     for(int i=0;i<n;i++){
         cin>>ar[i];
     }
